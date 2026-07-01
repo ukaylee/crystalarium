a5f5f01f8bdc8048cb02576f657d748309315f86
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-01 20:04:56 +0000

    more troubleshooting

--- services.py
+++ services.py
@@ -1,18 +1,21 @@
+import os
 import json
 from Crystal import Crystal
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def load_crystals():
-  with open("crystals.json") as f:
+    path = os.path.join(BASE_DIR, "crystals.json")
+
+    with open(path, "r") as f:
         data = json.load(f)
 
-  crystals = []
-  for item in data:
-      crystals.append(
-          Crystal(
-              item["id"],
-              item["name"],
-              item["props"],
-              item["desc"]
-          )
-      )
-  return crystals
+    return [
+        Crystal(
+            item["id"],
+            item["name"],
+            item["props"],
+            item["desc"]
+        )
+        for item in data
+    ]
