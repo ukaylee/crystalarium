0dacbdad7eb674c70ae687d7b4eb3c3c05e2e205
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-01 18:57:22 +0000

    adding user ORM stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 # from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
 from services import load_crystals
 from forms import RegistrationForm
 import git
@@ -7,6 +8,21 @@ import git
 app = Flask(__name__)
 # proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = "0d6a7916714408bffc2c5ceb0d976294" #need to put this in .env
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
 
 @app.route("/")
 
@@ -36,6 +52,11 @@ def register():
     if form.validate_on_submit(): # checks if entries are valid
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
+
+        #new stuff from day 3
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
     return render_template('register.html', form=form)
 
 # @app.route("/update_server", methods=['POST'])
