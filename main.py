2f7db5573819f74cb29df740ba95895ebfb7f594
Kaylee Ulep <kaylee.ulep@gmail.com>
2026-07-01 16:50:40 +0000

    setting up pythonanywhere

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_behind_proxy import FlaskBehindProxy
 from services import load_crystals
 from forms import RegistrationForm
+import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
-app.config['SECRET_KEY'] = "0d6a7916714408bffc2c5ceb0d976294"
+app.config['SECRET_KEY'] = "0d6a7916714408bffc2c5ceb0d976294" #need to put this in .env
 
 @app.route("/")
 
@@ -36,6 +37,16 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/ukaylee/crystalarium')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 if __name__ == '__main__':
